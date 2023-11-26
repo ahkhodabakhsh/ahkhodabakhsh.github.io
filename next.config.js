65230dab3e7ed9addd4938f16e33759c1214c365
AHKh <a.h.khodabakhsh@gmail.com>
2023-11-26 19:12:44 +0330

    removed the next js configs

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    // basePath:"/ahkhodabakhsh.github.io",
-    // assetPrefix:"/ahkhodabakhsh.github.io/",
-    basePath:"./",
-    assetPrefix:"/ahkhodabakhsh.github.io/",
 }
 
 module.exports = nextConfig
